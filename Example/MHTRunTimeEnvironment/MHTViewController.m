acb61e6a8961f19df7c519ecd8380b4e3744710e
mangox <329735967@qq.com>
2025-09-29 11:17:09 +0800

    0.1.1

--- Example/MHTRunTimeEnvironment/MHTViewController.m
+++ Example/MHTRunTimeEnvironment/MHTViewController.m
@@ -18,6 +18,8 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    NSString *proxy = [MHTRunTimeEnvironment sharedInstance].proxyURL;
+    NSLog(@"%@", proxy);
 }
 
 - (void)didReceiveMemoryWarning
